94c4402966f2ce1031497b6580bdd2dda6dad8e8
justine jones <justinejones@justines-MacBook-Air.local>
2014-06-24 18:50:54 -0400

    Writing the reviews routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :reviews, :only => [:show, :create, :destroy]
+
   resources :sessions, :only => [:new, :create, :destroy]
 
   resources :users, :only => [:new, :create]
