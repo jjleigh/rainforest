b9a5831e3d7131afdb5a2d120bb8933dfb5633ea
justine jones <justinejones@justines-MacBook-Air.local>
2014-07-09 17:48:20 -0400

    finished setting up ajax part 1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,9 @@ class ProductsController < ApplicationController
     else
       Product.all
     end
-
-    if request.xhr?
-      render @products
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
