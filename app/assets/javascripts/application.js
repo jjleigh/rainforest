57f0af074fcfe01ad5950f6cf4d81be0e7d99e7a
justine jones <justinejones@justines-MacBook-Air.local>
2014-07-10 15:32:43 -0400

    removed duplicate js from product.js, removed looping bug with products partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-$.getScript('/products?search=' + searchValue);
+// $(document).on('ready page: load', function() {
+// 	if ($('.pagination').length) {
+// 		$(window).scroll(function() {
+// 			var url = $('.pagination span.next').children().attr('href');
+// 			if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50)
+// 				$('.pagination').text('Fetching more products...');
+// 				return $.getScript(url);
+// 			}
+// 	});
+// });
+// $.getScript('/products?search=' + searchValue);
