8e0a7a08ed05e499eeebed99a470f36a467bdd2d
justine jones <justinejones@justines-MacBook-Air.local>
2014-07-10 14:31:09 -0400

    added the infinite scroll functionality

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,14 +3,20 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).on('ready page:load', functon() {
-	$('#search-form').submit(function(event) {
-		event.preventDefault();
-		var searchValue = $('#search').val();
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+			console.log($('.pagination span.next').children().attr('href'));
+			$.getScript($('.pagination span.next').children().attr('href'));
+		}
+	});
+	// $('#search-form').submit(function(event) {
+	// 	event.preventDefault();
+	// 	var searchValue = $('#search').val();
 
-		$.get('/products?search=' +searchValue)
-			.done(function(data){
-				console.log(data);
-				$('#products').html(data);
-			});
-		});
+	// 	$.get('/products?search=' +searchValue)
+	// 		.done(function(data){
+	// 			console.log(data);
+	// 			$('#products').html(data);
+	// 		});
+	// 	});
 	});
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,16 @@
 class ProductsController < ApplicationController
   before_filter :ensure_logged_in, :only => [:show]
   def index
-  	@products = if params[:search]
-      Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
-    else
-      Product.all
-    end
+  	# if params[:search]
+   #    @products = Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
+   #  else
+   #    @products = Product.all
+   #  end
+
+   @products = Product.order('products.created_at DESC').page(params[:page])
+
     respond_to do |format|
-      format.html
+      format.html #allows the controller to respond to Javascript
       format.js
     end
   end
