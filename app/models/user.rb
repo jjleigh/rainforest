ad7ac968a0fd2d86adcfe342f1b642819b2ae964
Justine Jones <justinejones91@gmail.com>
2014-09-01 09:58:50 -0400

    removed user params from the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,4 @@ class User < ActiveRecord::Base
 	has_many :products, :through => :reviews
 
 	validates_presence_of :name
-
-	private 
-	def user_params
-		params.require(:user).permit(:email, :password, :passwword_confirmation, :name)
-	end 
 end
