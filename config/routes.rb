4dc5367e164f63502b667899e6ffe28c9baef4ea
Justine Jones <justinejones91@gmail.com>
2014-09-01 21:45:27 -0400

    added the log out route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'products#index'
-
+  get "log_out" => "sessions#destroy", :as => "log_out" 
+  
   resources :sessions, :only => [:new, :create, :destroy]
 
   resources :users, :only => [:new, :create]
